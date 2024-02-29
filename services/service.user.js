d9bbf452454fecde49cebe5c463136980e2458a6
duy366110 <duy366110@gmail.com>
2024-02-29 16:46:13 +0700

    #Commit user: Destroy user success.

--- services/service.user.js
+++ services/service.user.js
@@ -28,13 +28,26 @@ class ServiceUser {
     // Create user account
     async createUser(infor = {}) {
         try {
-            return await ModelUser.create({
+            await ModelUser.create({
                 fullName: infor.fullName,
                 email: infor.email,
                 password: infor.password,
                 phoneNumber: infor.phoneNumber,
                 address: infor.address
             })
+
+            return {status: true, message: "Create user success"};
+
+        } catch (err) {
+            return {status: false, message: err.message};
+        }
+    }
+
+    // Delete user account
+    async destroyUser(id="") {
+        try {
+            await ModelUser.deleteOne({_id: {$eq: id}});
+            return {status: true, message: "Destroy user success"};
 
         } catch (err) {
             return {status: false, message: err.message};
