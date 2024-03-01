29aa2ee7111cc4d3d920afcf666bf5297e7dd785
duy366110 <duy366110@gmail.com>
2024-03-01 14:30:24 +0700

    #Commit service update user.

--- controllers/controller.user.js
+++ controllers/controller.user.js
@@ -16,6 +16,16 @@ class ControllerUser {
         }
     }
 
+    async getUserById(req, res, next) {
+        try {
+            let { id } = req.params;
+            let user = await ServiceUser.getUserById(id);
+            return res.status(200).json({status: true, user});
+        } catch (err) {
+            return res.status(400).json({status: false, user});
+        }
+    }
+
 
     async newUserAccount(req, res, next) {
         const error = validator.validationResult(req);
@@ -68,6 +78,47 @@ class ControllerUser {
         }
         return res.status(200).json({status: true, message: "User new successs"});
     }
+
+    async updateUserAccount(req, res, next) {
+        const error = validator.validationResult(req);
+        const { id, fullName, email, phoneNumber, address, role } = req.body;
+
+        // Check error
+        if (!error.isEmpty()) {
+            let statusCode = 400;
+            switch(error.array()[0].msg) {
+                case "E-mail not empty":
+                    statusCode = 400;
+                    break
+
+                case "Length of phone number from 9 - 12. and is number":
+                case "Enter fullName atleast 5 characters and max 20 characters":
+                    statusCode = 424;
+                    break
+
+                case "Enter email is invalid, try again":
+                    statusCode = 420;
+                    break
+
+                case "Your input is not type of phone number":
+                default:
+                    statusCode = 423;
+                    break
+            }
+            return res.status(statusCode).json({status: false, message: error.array()[0].msg});
+        }
+
+        let {status} = await ServiceUser.updateUser({
+            id, fullName, email,
+            phoneNumber, address, role
+        })
+
+        if(!status) {
+            return res.status(400).json({status: false, message: "User new unsuccesss"});
+        }
+        return res.status(200).json({status: true, message: "User new successs"});
+    }
+
 
     async destroyUserAccount(req, res, next) {
         const { id } = req.body;
